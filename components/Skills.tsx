8ca767d1cd0b3c365bb4b976a5b2c42426f80443
Cedcomps <compagnon.ced@gmail.com>
2023-10-30 08:42:41 +0100

    skills fixed

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,18 +15,19 @@ export default function Skills({}: Props) {
         {SkillsData.map((skill, index) => (
           <div key={index} className='group relative flex cursor-pointer'>
             <motion.img
-              initial={{
-                opacity: 0,
-              }}
+              initial={{ opacity: 0}}
               transition={{ duration: 1 }}
-              whileInView={{ opacity: 1, x: 0 }}
+              whileInView={{ opacity: 1, x: 0}}
               src={skill.imageUrl}
               alt={skill.title}
-              className='rounded-full object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
+              className=' object-cover -scale-150 w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-500 ease-in-out'
             />
-            <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
-              <div className='flex items-center h-full justify-center'>
-                <p className='text-3xl font-bold text-black opacity-100'>
+            <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32  z-0'>
+              <div className='flex flex-col items-center h-full justify-center'>
+                <p className='text-md font-bold text-black opacity-100 text-center'>
+                  {skill.title}
+                </p>
+                <p className='text-2xl font-bold text-black opacity-100'>
                   {skill.progression}
                 </p>
               </div>
--- components/skillsData.ts
+++ components/skillsData.ts
@@ -8,12 +8,12 @@ interface Skill {
   
   const SkillsData: Skill[] = [
     {
-      title: "Google Cybersecurity Certificate",
+      title: "PSM1",
       imageUrl: "https://images.credly.com/size/340x340/images/a2790314-008a-4c3d-9553-f5e84eb359ba/image.png",
       progression: "97%",
     },
     {
-        title: "PSM1",
+        title: "Google Cybersecurity Certificate",
         imageUrl: "https://images.credly.com/size/340x340/images/0bf0f2da-a699-4c82-82e2-56dcf1f2e1c7/image.png",
         progression: "93%",
     },
@@ -38,15 +38,15 @@ interface Skill {
           progression: "90%",
         },
         {
-            title: "Canva",
-            imageUrl: "https://icon.icepanel.io/Technology/svg/Canva.svg",
-            progression: "100%",
+            title: "JavaScript",
+            imageUrl: "https://icon.icepanel.io/Technology/svg/JavaScript.svg",
+            progression: "60%",
         },   
-        {
-            title: "Visual Studio Code",
-            imageUrl: "https://icon.icepanel.io/Technology/svg/Visual-Studio-Code-%28VS-Code%29.svg",
-            progression: "30%",
-        },
+        // {
+        //     title: "Visual Studio Code",
+        //     imageUrl: "https://icon.icepanel.io/Technology/svg/Visual-Studio-Code-%28VS-Code%29.svg",
+        //     progression: "30%",
+        // },
         {
             title: "CSS3",
             imageUrl: "https://icon.icepanel.io/Technology/svg/CSS3.svg",
@@ -95,7 +95,7 @@ interface Skill {
         {
             title: "PHP",
             imageUrl: "https://icon.icepanel.io/Technology/svg/PHP.svg",
-            progression: "90%",
+            progression: "70%",
         },
         {
             title: "PuTTY",
@@ -107,11 +107,11 @@ interface Skill {
             imageUrl: "https://icon.icepanel.io/Technology/svg/Figma.svg",
             progression: "30%",
         },
-        {
-            title: "PHP",
-            imageUrl: "https://icon.icepanel.io/Technology/svg/PHP.svg",
-            progression: "90%",
-        },
+        // {
+        //     title: "PHP",
+        //     imageUrl: "https://icon.icepanel.io/Technology/svg/PHP.svg",
+        //     progression: "90%",
+        // },
     // Ajoutez d'autres projets de la même manière
   ];
   
